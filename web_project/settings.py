5b35859250703ebe9141b4ed996f15ccd3198647
Coview2001 <kecoview@gmail.com>
2024-08-20 16:41:18 +0530

    2push

--- web_project/settings.py
+++ web_project/settings.py
@@ -66,10 +66,10 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500', 
     'http://127.0.0.1:5501',
     'http://localhost:5173', 
-    #'https://surgemeetlink.azurewebsites.net/',
+    'https://surgemeetlink.azurewebsites.net/',
 ]
 
-CSRF_TRUSTED_ORIGINS=[ 'http://localhost:3000' ]#,'https://surgemeetlink.azurewebsites.net/']
+CSRF_TRUSTED_ORIGINS=[ 'http://localhost:3000' ,'https://surgemeetlink.azurewebsites.net/']
 
 ROOT_URLCONF = 'web_project.urls'
 
