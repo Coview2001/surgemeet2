85ed253253532202dbf9c9470a2dd5e20f2c2eae
Coview2001 <kecoview@gmail.com>
2024-08-28 07:16:31 +0000

    clear

--- meetsessions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Session,Student
-
-# Register your models here.
-admin.site.register(Session)
-admin.site.register(Student)
--- trainer/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add_trainers/', views.add_user, name='add_user'),
-    path('get_trainers/', views.get_all_users, name='admin_users'),
-]
