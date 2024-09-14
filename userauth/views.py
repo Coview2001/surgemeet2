a034671dc107931d4c253c461d1fcc4f262b5434
Coview2001 <kecoview@gmail.com>
2024-09-14 10:12:30 +0530

    push12

--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -28,37 +29,39 @@ def login_with_google(request):
     if 'code' in request.data.keys():
         code = request.data['code']
         print("step1 received code\t", code)
-        credentials = utils.get_id_token_with_code_method_2(code)
+        credentials = utils.get_id_token_with_code_method_1(code)
         print("step 2: credentials\n", credentials)
-        id_token = credentials.id_token
-        access_token = {
-            "access_token": credentials.access_token,
-            "refresh_token": credentials.refresh_token,
-            "token_uri": credentials.token_uri,
-            "client_id": credentials.client_id,
-            "client_secret": credentials.client_secret,
-            "scopes": list(credentials.scopes),
-            "token_expiry": credentials.token_expiry.isoformat() if credentials.token_expiry else None,
-            "id_token": credentials.id_token,
-        }
-        print("received access token also \n\n\n\n", access_token)
-        user_email = id_token['email']
-        user_name = id_token['name']
-        user_pic = id_token['picture']
-        user_id = authenticate_or_create_user(user_email)
-        if user_id['exist']:
-            UserLogin.save_or_update(user_email, access_token)
-            return Response({
-                "Userid": user_id['userID'],
-                "email": user_email,
-                "user_name": user_name,
-                "user_picture": user_pic,
-                "user_cat": user_id['user_cat'],
-                "exists": True
-            })
-        else:
-            print("User not found")
-            return Response({"message": user_id['message'], "exists": False})
+        if(credentials):
+            return JsonResponse({"message":credentials})
+        # id_token = credentials.id_token
+        # access_token = {
+        #     "access_token": credentials.access_token,
+        #     "refresh_token": credentials.refresh_token,
+        #     "token_uri": credentials.token_uri,
+        #     "client_id": credentials.client_id,
+        #     "client_secret": credentials.client_secret,
+        #     "scopes": list(credentials.scopes),
+        #     "token_expiry": credentials.token_expiry.isoformat() if credentials.token_expiry else None,
+        #     "id_token": credentials.id_token,
+        # }
+        # print("received access token also \n\n\n\n", access_token)
+        # user_email = id_token['email']
+        # user_name = id_token['name']
+        # user_pic = id_token['picture']
+        # user_id = authenticate_or_create_user(user_email)
+        # if user_id['exist']:
+        #     UserLogin.save_or_update(user_email, access_token)
+        #     return Response({
+        #         "Userid": user_id['userID'],
+        #         "email": user_email,
+        #         "user_name": user_name,
+        #         "user_picture": user_pic,
+        #         "user_cat": user_id['user_cat'],
+        #         "exists": True
+        #     })
+        # else:
+        #     print("User not found")
+        #     return Response({"message": user_id['message'], "exists": False})
 
     return Response(status=status.HTTP_400_BAD_REQUEST)
 class ClearUserLoginData(APIView):
