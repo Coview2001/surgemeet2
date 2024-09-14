8abed3a67aa97a9f8c3f544b13f6342030ba9440
Coview2001 <kecoview@gmail.com>
2024-09-14 10:39:01 +0530

    push13

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ClearUserLoginData,login_with_google
+from .views import ClearUserLoginData,LoginWithGoogle
 from .userviews import get_user_details,add_user_details
 
 urlpatterns = [
-    path('login-with-google/', login_with_google,name=('login-with-google')),
+    path('login-with-google/', LoginWithGoogle.as_view(),name=('login-with-google')),
     path('add-user/', add_user_details, name='add_user_details'),
     path('get-users/', get_user_details, name='get_user_details'),
     path('clear_user_login/', ClearUserLoginData.as_view(), name='clear_user_login'),
