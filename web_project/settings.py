aee51687a6ecdc0349a9ff0640714a9fdff40a4f
Coview2001 <kecoview@gmail.com>
2024-09-24 13:39:05 +0530

    allmightypushcv3

--- web_project/settings.py
+++ web_project/settings.py
@@ -85,12 +85,12 @@ CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500', 
     'http://127.0.0.1:5501',
     'http://localhost:5173', 
-    # 'https://surgemeetlink.azurewebsites.net/',
+    'https://surgemeetlink.azurewebsites.net/',
 ]
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:3000',
-#     'https://surgemeet.azurewebsites.net/',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://surgemeet.azurewebsites.net/',
+]
 
 
 CSRF_TRUSTED_ORIGINS=[ 'http://localhost:3000' ,'https://surgemeetlink.azurewebsites.net/']
